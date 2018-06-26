63ffec616f0b1ffe5c82d859c89af03813916d96
WillChew <williamchew95@gmail.com>
2018-06-26 15:52:16 -0400

    finished optionals

--- W4D2_Playground.playground/Pages/More Optionals.xcplaygroundpage/Contents.swift
+++ W4D2_Playground.playground/Pages/More Optionals.xcplaygroundpage/Contents.swift
@@ -7,24 +7,39 @@
  - Experiment:
  Declare a optional Double value and set it to nil.
  */
+var optionalDouble : Double? = nil
+
+
 
 
 /*:
  - Experiment:
  Assign a value your optional Double.
+ 
  */
-
+optionalDouble = 10
 
 /*:
  - Experiment:
  Force unwrap the optional value. Why do you have to be careful about force unwrapping?
  */
-
+if optionalDouble! == 10.0 {
+    print(optionalDouble!)
+}
 
 /*:
  - Experiment:
  Use conditional unwrapping to verify if the optional has a value. Print the value if there is something, otherwise, print out to indicate there is no value present. Why is conditional unwrapping better than force unwrapping?
  */
+
+if let unwrapped = optionalDouble {
+    print(unwrapped)
+} else {
+    print("There is no value")
+}
+
+//safer because if there is somethign there it will print it else, it will get out of the func
+//if there is no value and I force unwrap it it wi
 
 
 /*:
@@ -33,6 +48,9 @@
  */
 var testData: [String?] = ["Heather", nil, "Mike", "John", nil, nil, "Bob"]
 
+//func removeArray () {
+//    let compactmapped : [String] = testData.map(String)
+//}
 
 /*:
  - Callout(Challenge):
@@ -41,20 +59,79 @@ var testData: [String?] = ["Heather", nil, "Mike", "John", nil, nil, "Bob"]
  Below is some test data you can use to test your function.
  */
 // Should pass all checks and print all information
-let username: String? = "user1"
-let password: String? = "password123"
-let email: String? = "user1@lighthouselabs.ca"
+//let username: String? = "user1"
+//let password: String? = "password123"
+//let email: String? = "user1@lighthouselabs.ca"
+//
+//if let username = username {
+//    print(username)
+//} else {
+//    print ("blank")
+//}
+//if let password = password {
+//    print (password)
+//} else {
+//    print ("blank")
+//}
+//if let email = email {
+//    print(email)
+//} else {
+//    print("blank")
+//}
 
 // Should stop at password check and indicate password field is empty
 //let username: String? = "user1"
 //let password: String? = nil
 //let email: String? = "user1@lighthouselabs.ca"
+//
+//func checkUser (username : String?, password : String?, email : String?) {
+//    if  let username = username {
+//        print(username)
+//    } else {
+//        return
+//    }
+//    if  let password = password {
+//        print(password)
+//    } else {
+//        print("empty")
+//        return
+//    }
+//
+//    if  let email = email {
+//        print(email)
+//    } else {
+//        return
+//    }
+//}
+//checkUser(username: username, password: password, email: email)
 
 // Should stop at username check and indicate username field is empty
-//let username: String? = nil
-//let password: String? = nil
-//let email: String? = "user1@lighthouselabs.ca"
+let username: String? = nil
+let password: String? = nil
+let email: String? = "user1@lighthouselabs.ca"
 
+func checkUser (username : String?, password : String?, email : String?) {
+    if  let username = username {
+        print(username)
+    } else {
+        print("empty")
+        return
+    }
+    if  let password = password {
+        print(password)
+    } else {
+        print("empty")
+        return
+    }
+
+    if  let email = email {
+        print(email)
+    } else {
+        print("empty")
+        return
+    }
+}
+checkUser(username: username, password: password, email: email)
 
 
 /*:
@@ -67,16 +144,16 @@ let myNumber = 10
 
 // This function checks if the number given to it is a positive or negative value
 func isMyNumberANegativeValue(myNumber: Int){
-  
-  // This guard condition checks if 'myNumber' is less than zero.
-  // Remember, if this condition IS FALSE, the block of code within the else block will run
-  // Otherwise, the function will continue
-  guard myNumber < 0 else {
-    print("I am positive")
-    return
-  }
-  
-  print("I am negative!")
+    
+    // This guard condition checks if 'myNumber' is less than zero.
+    // Remember, if this condition IS FALSE, the block of code within the else block will run
+    // Otherwise, the function will continue
+    guard myNumber < 0 else {
+        print("I am positive")
+        return
+    }
+    
+    print("I am negative!")
 }
 
 isMyNumberANegativeValue(myNumber: myNumber)
@@ -86,11 +163,34 @@ isMyNumberANegativeValue(myNumber: myNumber)
  Try creating your own guard statement with different conditional statements. Notice which boolean condition causes the code the enter the 'else' block or bypass it entirely.
  */
 
+func isMyNumberAPositiveNumbe(myNumber: Int) {
+    guard myNumber > 0 else {
+        print("I am negative")
+        return
+    }
+    print("I am positive!")
+}
+
+isMyNumberAPositiveNumbe(myNumber: myNumber)
+
 
 /*:
  - Experiment:
  Create a function that takes in two number parameters and divide them. We don't like dividing by zero, so ensure this doesn't happen. Otherwise, return the calculated value.
  */
+
+let numberOne = 20
+let numberTwo = 10
+
+func checkForZero (numberOne: Int, numberTwo: Int) -> Int? {
+    guard numberOne != 0, numberTwo != 0 else {
+        print("Can not do this")
+        return nil
+    }
+    return (numberOne/numberTwo)
+}
+
+checkForZero(numberOne: numberOne, numberTwo: numberTwo)
 
 
 /*:
@@ -101,20 +201,20 @@ let myOptionalNumber: Int? = 10
 
 // This function just checks if the given parameter is nil or not
 func isMyNumberAnOptional(myOptionalNumber: Int?){
-  
-  // This guard checks if 'myOptionalNumber' has a value or not. Similar concept to conditional unwrapping
-  // If 'myOptionalNumber' is indeed nil, the else block will run
-  // Otherwise, our guard let statement is looking for a valid value, and the code will continue to run
-  guard let myUnwrapped = myOptionalNumber else {
     
-    print("This value is nil")
-    return
-  }
-  
-  // Lastly, you can see we can use the variable 'myUnwrapped' variable declared above within our entire scope of this function now
-  // Very convenient!!
-  // A regular 'if let' would not allow this
-  print("This unwrapped value is \(myUnwrapped)")
+    // This guard checks if 'myOptionalNumber' has a value or not. Similar concept to conditional unwrapping
+    // If 'myOptionalNumber' is indeed nil, the else block will run
+    // Otherwise, our guard let statement is looking for a valid value, and the code will continue to run
+    guard let myUnwrapped = myOptionalNumber else {
+        
+        print("This value is nil")
+        return
+    }
+    
+    // Lastly, you can see we can use the variable 'myUnwrapped' variable declared above within our entire scope of this function now
+    // Very convenient!!
+    // A regular 'if let' would not allow this
+    print("This unwrapped value is \(myUnwrapped)")
 }
 
 isMyNumberAnOptional(myOptionalNumber: myOptionalNumber)
@@ -123,6 +223,21 @@ isMyNumberAnOptional(myOptionalNumber: myOptionalNumber)
  - Experiment:
  Create a function that takes in an array of numbers. Have the function add all the numbers together and return the result. Make sure to `guard` against an empty array. Use `array.first` to check if there is at least one value in the array.
  */
+let numberArray : [Int] = [1,2,3,4,5]
+var sum : Int = 0
+
+func checkNumbers(Array : [Int]) -> Int? {
+    guard Array.first != nil else {
+        print("nil")
+        return nil
+    }
+    for numbers in numberArray {
+        sum += numbers
+    }
+    return sum
+}
+
+checkNumbers(Array: numberArray)
 
 
 /*:
